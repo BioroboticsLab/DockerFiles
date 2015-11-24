1223dfd8eeaea57ba74c9f809e70b0e72b41244b
Benjamin Wild <b.w@fu-berlin.de>
2015-11-24 14:24:43 +0100

    remove caffe and cudnn to fix DockerHub build

--- arch-linux-biorobotics/build.sh
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env bash
-
-CUDNN="cudnn-7.0-linux-x64-v3.0-prod.tgz"
-if [ ! -e "$CUDNN" ]; then
-    echo "Download CUDNN $CUDNN from the NVIDIA homepage and put it into this directory."
-    exit 1
-fi
-
-docker build -t biorobotics/arch .
