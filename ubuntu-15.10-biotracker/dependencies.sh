e364f0c069803f63c10ac4948ed585ed8ac12204
Michael Wittig <michael.wittig@fu-berlin.de>
2015-12-08 12:23:34 +0100

    fixed missing linefeed

--- ubuntu-15.10-biotracker/dependencies.sh
+++ ubuntu-15.10-biotracker/dependencies.sh
@@ -17,16 +17,8 @@ apt-get update && apt-get install -y \
     libswscale-dev \
     qt5-default \
     libopencv-dev \
-    libc++abi-dev
-    libboost-all-dev \
-
-#cd /usr/local
-#wget http://downloads.sourceforge.net/project/boost/boost/1.59.0/boost_1_59_0.tar.bz2
-#tar --bzip2 -xf boost_1_59_0.tar.bz2
-#rm boost_1_59_0.tar.bz2
-#cd boost_1_59_0
-#./bootstrap.sh
-#./b2 install
+    libc++abi-dev \
+    libboost-all-dev
 
 # PYTHON deps
 apt-get install -y python-dev
